42ed17eacd1a12e1b6111c1c53fd050484e24f6c
Nishant Sinha <nishant@offnote.co>
2023-09-05 14:40:58 +0530

    files

---
+++ langchoice/common.py
@@ -0,0 +1,8 @@
+class DD(dict):
+    """
+    Access dictionary attributes via dot notation
+    """
+
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
